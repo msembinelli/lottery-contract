1d2ee3617aa84450d85b5658b237309c256b1001
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2018-02-10 22:29:22 -0700

    Add test for entering with one account

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -8,22 +8,36 @@ const web3 = new Web3(provider);
 const { interface, bytecode } = require('../compile');
 
 let accounts;
-let inbox;
+let lottery;
 
 beforeEach(async () => {
     // Get a list of all accounts
     accounts = await web3.eth.getAccounts();
 
     // Use one of those accounts to deploy the contract
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    lottery = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode })
         .send({ from: accounts[0], gas: '1000000' });
 
-    inbox.setProvider(provider);
+    lottery.setProvider(provider);
 });
 
 describe('Lottery Contract', () => {
     it('deploys a contract', () => {
-        assert.ok(inbox.options.address);
+        assert.ok(lottery.options.address);
+    });
+
+    it('allows one account to enter', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(accounts[0], players[0]);
+        assert.equal(1, players.length);
     });
 });
